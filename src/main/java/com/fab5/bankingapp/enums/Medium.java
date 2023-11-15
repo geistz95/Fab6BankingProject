b8a99ad4b0e7ae7cf918f0a0eadacce02052e226
Ki <kfchan93@gmail.com>
2023-11-15 13:38:46 -0500

    Coded Deposit model, and added TransactType and Medium Enum

--- src/main/java/com/fab5/bankingapp/enums/Medium.java
+++ src/main/java/com/fab5/bankingapp/enums/Medium.java
@@ -5,4 +5,16 @@ import javax.persistence.Entity;
 @Entity
 public enum Medium {
     //Specifies how the transaction is being handled. Balance or Rewards
+    BALANCE("balance"),REWARDS("rewards");
+
+    private final String medium;
+
+    Medium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
 }
