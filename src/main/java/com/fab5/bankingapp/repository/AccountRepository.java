bed02722e059b0afcedeb2a9584022c6388a1bce
Yaw <baffourampadu06@gmail.com>
2023-11-16 15:49:09 -0500

    Add First draft of all classes

--- src/main/java/com/fab5/bankingapp/repository/AccountRepository.java
+++ src/main/java/com/fab5/bankingapp/repository/AccountRepository.java
@@ -1,7 +1,18 @@
 package com.fab5.bankingapp.repository;
 
+import com.fab5.bankingapp.enums.AccountType;
+import com.fab5.bankingapp.model.Account;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class AccountRepository {
+public interface AccountRepository extends CrudRepository<Account, Long>{
+
+    List<Account> findByCustomerId(Long customerId);
+
+
+
+
 }
