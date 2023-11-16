d1708d8c756b9c491254ab95ba86bbf0f8932ddc
tejaswini1024 <aswi1024@gmail.com>
2023-11-16 11:33:05 -0500

    chanes in controller

--- src/main/java/com/fab5/bankingapp/controller/CustomerController.java
+++ src/main/java/com/fab5/bankingapp/controller/CustomerController.java
@@ -1,11 +1,40 @@
 package com.fab5.bankingapp.controller;
 
-import com.fab5.bankingapp.service.CustomerService;
+import com.fab5.bankingapp.model.Customer;
+import com.fab5.bankingapp.service.CustomerService;;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Optional;
+
+@RequestMapping("/customers")
+@RestController
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+    @GetMapping
+    public Iterable<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Customer> getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+   /* @GetMapping("/accounts/{accountId}/customer")
+    public Optional<Customer> getCustomerByAccountId(@PathVariable Long accountId) {
+        return customerService.getCustomerByAccountId(accountId);
+    }*/
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer updatedCustomer) {
+        return customerService.updateCustomer(id, updatedCustomer);
+    }
 }
--- src/main/java/com/fab5/bankingapp/service/CustomerService.java
+++ src/main/java/com/fab5/bankingapp/service/CustomerService.java
@@ -12,6 +12,34 @@ public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+    public Iterable<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Long id, Customer updatedCustomer) {
+        if (customerRepository.existsById(id)) {
+            updatedCustomer.setId(id);
+            return customerRepository.save(updatedCustomer);
+        } else {
+            // Handle the case where the customer with the given id is not found
+            return null;
+        }
+    }
+
+    public Optional<Customer> getCustomerByAccountId(Long accountId) {
+        // Add logic to retrieve the customer that owns the specified account
+        // This might involve querying the database based on the account information
+        // For demonstration purposes, let's assume the account information is stored in the Customer entity
+        return customerRepository.findByAccountId(accountId);
+    }
 }
 
 
