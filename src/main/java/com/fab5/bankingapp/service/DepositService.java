047a922f782ef1d7f89323a4727fbb868f6ffb7d
Ki <kfchan93@gmail.com>
2023-11-16 17:19:46 -0500

    Added verifies to the deposit service layer

--- src/main/java/com/fab5/bankingapp/service/DepositService.java
+++ src/main/java/com/fab5/bankingapp/service/DepositService.java
@@ -25,7 +25,15 @@ public class DepositService {
             throw new DepositNotFoundException();
         }
     }
+
+    public void verifyCustomerID(Long customerID){
+        if(customerRepository.existsById(customerID)){
+            throw new CustomerDoesNotExistException();
+        }
+    }
+
     public Optional<Deposit> getDepositByID(Long id){
+        verifyDepositID(id);
         return depositRepository.findById(id);
     }
 
@@ -34,6 +42,7 @@ public class DepositService {
     }
 
     public void editDeposit(Deposit deposit, Long id){
+        verifyDepositID(id);
         Deposit oldDeposit = depositRepository.findById(id).get();
         oldDeposit.setAmount(deposit.getAmount());
         oldDeposit.setDescription(deposit.getDescription());
@@ -43,14 +52,15 @@ public class DepositService {
         depositRepository.save(oldDeposit);
     }
 
-    public void deleteDeposit(Deposit deposit){
+    public void deleteDeposit(Deposit deposit, Long id){
+        verifyDepositID(id);
         depositRepository.delete(deposit);
     }
 
-    public void deleteDepositByID(Long id){
-        depositRepository.deleteById(id);
-    }
 
-    public Iterable<Deposit> getAllDepositsByCustomerID(Long id){return depositRepository.findAllDepositsByCustomerID(id);}
+    public Iterable<Deposit> getAllDepositsByCustomerID(Long customerId){
+        verifyCustomerID(customerId);
+        return depositRepository.findAllDepositsByCustomerID(customerId);
+    }
 
 }
