db9904685c777110d47e6b4966a9cd15171f715b
tejaswini1024 <aswi1024@gmail.com>
2023-11-16 22:19:46 -0500

    changes in customerclasses

--- src/main/java/com/fab5/bankingapp/repository/CustomerRepository.java
+++ src/main/java/com/fab5/bankingapp/repository/CustomerRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    Optional<Customer> findByAccountId(Long accountId);
+   // Optional<Customer> findById(Long accountId);;
+   Iterable<Customer> findCustomerByAccountId(Long accountId);
 }
