24a9e1ac1affb2b2a1ffdd8a9be1ef7b96e66ec6
Yaw <baffourampadu06@gmail.com>
2023-11-17 21:30:05 -0500

    Add all changes

--- src/main/java/com/fab5/bankingapp/enums/AccountType.java
+++ src/main/java/com/fab5/bankingapp/enums/AccountType.java
@@ -5,11 +5,15 @@ public enum AccountType {
     // This will specify what type of account it is, Checking, Savings, etc.
 
 
-         Savings("SAVINGS"), Credit("CREDIT"), Cheque("CHEQUE"), JointAccount("JOINTACCOUNT");
+         Savings("SAVINGS"), Credit("CREDIT"), Cheque("CHEQUE"), JointAccount("JOINTACCOUNT"), Rewards("REWARD");
 
          private final String accountType;
 
     AccountType(String accountType) {
         this.accountType = accountType;
+    }
+
+    public String getAccountType() {
+        return accountType;
     }
 }
