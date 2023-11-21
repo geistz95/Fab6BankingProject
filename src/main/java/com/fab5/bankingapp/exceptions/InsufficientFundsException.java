664f86a75ceb07828d930a6c053a75ed180c5ea3
Itsonedev <devonbrownjr24@gmail.com>
2023-11-21 00:54:16 -0500

    Transaction Service

---
+++ src/main/java/com/fab5/bankingapp/exceptions/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package com.fab5.bankingapp.exceptions;
+
+public class InsufficientFundsException extends RuntimeException {
+
+    public InsufficientFundsException(String message){
+        super(message);
+    }
+}
