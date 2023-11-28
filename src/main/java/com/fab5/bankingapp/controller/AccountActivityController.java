c6d92ba5952e0b07c89423ab33e2c5f6c73289ce
tejaswini1024 <aswi1024@gmail.com>
2023-11-28 10:46:25 -0500

    account activity

--- src/main/java/com/fab5/bankingapp/controller/AccountActivityController.java
+++ src/main/java/com/fab5/bankingapp/controller/AccountActivityController.java
@@ -28,15 +28,6 @@ public class AccountActivityController {
     @Autowired
     private AccountActivityService accountActivityService;
 
-    @PostMapping("/account-activities/{accountId}")
-    public ResponseEntity<?> addAccountActivities(@RequestBody AccountActivity accountActivity, @PathVariable Long accountId) {
-        logger.info("Received request to add account activity for account ID: {}", accountId);
-
-        accountActivityService.saveAccountActivities(accountActivity);
-
-        logger.info("Account activity added successfully for account ID: {}", accountId);
-        return AccountActivityResponse.createdActivityBuilder(HttpStatus.CREATED, accountActivity);
-    }
 
     @GetMapping("/account-activities/{accountId}")
     public ResponseEntity<?> getAccountActivities(@PathVariable Long accountId) {
@@ -53,29 +44,5 @@ public class AccountActivityController {
         return AccountActivityResponse.getActivityBuilder(HttpStatus.OK, activityList.orElse(null));
     }
 
-    @PutMapping("/account-activities/{accountId}")
-    public ResponseEntity<?> updateAccountActivities(@RequestBody AccountActivity activity, @PathVariable Long accountId) {
-        logger.info("Received request to update account activity for account ID: {}", accountId);
 
-        accountActivityService.updateAccountActivities(activity, accountId);
-
-        logger.info("Account activity updated successfully for account ID: {}", accountId);
-        return AccountActivityResponse.putActivityBuilder(HttpStatus.OK);
-    }
-
-    @DeleteMapping("{accountId}")
-    public ResponseEntity<?> deleteAccountActivities(@PathVariable Long accountId) {
-        logger.info("Received request to delete account activities for account ID: {}", accountId);
-
-        Optional<AccountActivity> activity = accountActivityService.getAccountActivities(accountId);
-        accountActivityService.deleteAccountActivities(accountId);
-
-        if (activity.isPresent()) {
-            logger.info("Deleted account activities successfully for account ID: {}", accountId);
-        } else {
-            logger.info("No account activities found for account ID: {}", accountId);
-        }
-
-        return AccountActivityResponse.deleteActivityBuilder(HttpStatus.NO_CONTENT, activity);
-    }
 }
--- src/main/java/com/fab5/bankingapp/response/AccountActivityResponse.java
+++ src/main/java/com/fab5/bankingapp/response/AccountActivityResponse.java
@@ -11,13 +11,7 @@ import java.util.Optional;
 
 public class AccountActivityResponse {
 
-    public static ResponseEntity<Object> createdActivityBuilder(HttpStatus httpStatus, Object responseObject) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Successfully created the account activity");
-        response.put("data result", responseObject);
-        response.put("HttpStatus Code", httpStatus);
-        return new ResponseEntity<>(response, httpStatus);
-    }
+
 
     public static ResponseEntity<Object> getActivityBuilder(HttpStatus httpStatus, Object object) {
         Map<String, Object> response = new HashMap<>();
@@ -26,28 +20,7 @@ public class AccountActivityResponse {
         return new ResponseEntity<>(response, httpStatus);
     }
 
-    public static ResponseEntity<Object> getAllActivitiesBuilder(HttpStatus httpStatus, List<?> object) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("HttpStatus Code", httpStatus);
-        response.put("data", object);
-        return new ResponseEntity<>(response, httpStatus);
-    }
 
-    public static ResponseEntity<Object> deleteActivityBuilder(HttpStatus httpStatus, Optional<AccountActivity> activity) {
-        Map<String, Object> response = new HashMap<>();
-        if (activity.isPresent()) {
-            response.put("message", "Activity has been successfully deleted");
-        } else {
-            response.put("message", "Activity does not exist");
-        }
-        response.put("HttpStatus Code", httpStatus);
-        return new ResponseEntity<>(response, httpStatus);
-    }
 
-    public static ResponseEntity<Object> putActivityBuilder(HttpStatus httpStatus) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("HttpStatus code", httpStatus);
-        response.put("message", "Accepted activity modification");
-        return new ResponseEntity<>(response, httpStatus);
-    }
+
 }
