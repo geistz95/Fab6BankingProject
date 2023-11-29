440442e100358080fb90f8fe8906e623d4ead002
tejaswini1024 <aswi1024@gmail.com>
2023-11-28 20:46:54 -0500

    change in bill

--- src/main/java/com/fab5/bankingapp/repository/AccountActivityRepository.java
+++ src/main/java/com/fab5/bankingapp/repository/AccountActivityRepository.java
@@ -8,7 +8,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AccountActivityRepository extends JpaRepository<AccountActivity, Long> {
-    @Query(value = "SELECT x.* FROM accountactivity x JOIN account a ON x.account_id = a.account_id WHERE a.account_id = ?1")
+
+    @Query(value = "SELECT x.* FROM account_activity x JOIN Account a ON x.account_id = a.account_id WHERE x.activity_id = ?1", nativeQuery = true)
     List<AccountActivity> findByAccountId(Long accountId);
+//    @Query(value = "SELECT x.* FROM accountactivity x JOIN account a ON x.account_id = a.account_id WHERE a.account_id = ?1")
+
 
 }
