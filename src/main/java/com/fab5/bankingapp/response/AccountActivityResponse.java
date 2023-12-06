ba412a73dfac3478629ec9679c80faf6d9dec225
Yaw <baffourampadu06@gmail.com>
2023-12-06 13:37:28 -0500

    Add format changes

--- src/main/java/com/fab5/bankingapp/response/AccountActivityResponse.java
+++ src/main/java/com/fab5/bankingapp/response/AccountActivityResponse.java
@@ -4,24 +4,21 @@ import com.fab5.bankingapp.model.AccountActivity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class AccountActivityResponse {
 
 
 
     public static ResponseEntity<Object> getActivityBuilder(HttpStatus httpStatus, Object object) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("HttpStatus Code", httpStatus);
         response.put("data result", object);
         return new ResponseEntity<>(response, httpStatus);
     }
 
     public static ResponseEntity<Object> getAllActivityBuilder(HttpStatus httpStatus, Object object){
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("HttpStatus Code", httpStatus);
         response.put("data result", object);
         return new ResponseEntity<>(response, httpStatus);
--- src/main/java/com/fab5/bankingapp/response/AccountResponse.java
+++ src/main/java/com/fab5/bankingapp/response/AccountResponse.java
@@ -4,37 +4,34 @@ import com.fab5.bankingapp.model.Account;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class AccountResponse {
 
     public static ResponseEntity<Object> createdAccountBuilder(HttpStatus httpStatus, Object responseObject) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("message", "Successfully created the account");
         response.put("data result", responseObject);
         response.put("HttpStatus Code", httpStatus);
-        return new ResponseEntity<>(response, httpStatus);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(httpStatus.value()));
     }
 
     public static ResponseEntity<Object> getAccountBuilder(HttpStatus httpStatus, Object object) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("HttpStatus Code", httpStatus);
         response.put("data result", object);
-        return new ResponseEntity<>(response, httpStatus);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(httpStatus.value()));
     }
 
     public static ResponseEntity<Object> getAllAccountsBuilder(HttpStatus httpStatus, List<?> object) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
         response.put("HttpStatus Code", httpStatus);
         response.put("data", object);
-        return new ResponseEntity<>(response, httpStatus);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(httpStatus.value()));
     }
 
     public static ResponseEntity<Object> deleteAccountBuilder(HttpStatus httpStatus) {
-        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> response = new LinkedHashMap<>();
 
        /* if (account.isPresent()) {
             response.put("message", "Account has been successfully deleted");
@@ -44,13 +41,13 @@ public class AccountResponse {
         response.put("HttpStatus Code", httpStatus);*/
 
         response.put("message", "Account has been successfully deleted");
-        return new ResponseEntity<>(response, httpStatus);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(httpStatus.value()));
     }
 
     public static ResponseEntity<Object> putAccountBuilder(HttpStatus httpStatus) {
-        Map<String, Object> response = new HashMap<>();
+        LinkedHashMap<String, Object> response = new LinkedHashMap<>();
         response.put("HttpStatus code", httpStatus);
         response.put("message", "Accepted account modification");
-        return new ResponseEntity<>(response, httpStatus);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(httpStatus.value()));
     }
 }
